39780cebca2a5b21ffaf2df408118758492678f5
qinjie <156860180@qq.com>
2016-12-08 13:11:05 +0800

    新闻列表数据获取成功

--- app/src/main/java/com/example/katherine_qj/xiyou_library/view/fragment/fragtmentNews.java
+++ app/src/main/java/com/example/katherine_qj/xiyou_library/view/fragment/fragtmentNews.java
@@ -3,6 +3,8 @@ package com.example.katherine_qj.xiyou_library.view.fragment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,18 +19,15 @@ import com.example.katherine_qj.xiyou_library.presenter.fragementNewsPresenter;
 
 import java.util.ArrayList;
 import java.util.List;
-
 /**
  * Created by Katherine-qj on 2016/12/3.
  */
-
 public class fragtmentNews extends Fragment implements IfragmentNews{
     private View view;
     private RecyclerView recyclerView_news;
     private List<News> list = new ArrayList<>();
     private RecycleViewAdapter recycleViewAdapter ;
     private fragementNewsPresenter fragementNewsPresenter; ;
-
     private ToastMassage toastMassage = new ToastMassage();
 
     @Override
@@ -42,18 +41,24 @@ public class fragtmentNews extends Fragment implements IfragmentNews{
         toastMassage.showMassage("接口挂了吧还是什么乱七八糟的",getContext());
     }
 
+    @Override
+    public void setRecycleView(List<News> list) {
+        this.list = list;
+        recyclerView_news.setLayoutManager(new LinearLayoutManager(recyclerView_news.getContext()));
+        recyclerView_news.setAdapter(recycleViewAdapter = new RecycleViewAdapter(getContext(),list));
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = (View)inflater.inflate(R.layout.fragment_news,container,false);
         initView();
         fragementNewsPresenter.getNewsList("news",1);
-        //list = fragementNewsPresenter.getNewsList("news",1);
         return view;
     }
     public void initView(){
         recyclerView_news = (RecyclerView)view.findViewById(R.id.news_recyclerView);
         recycleViewAdapter = new RecycleViewAdapter(getContext(),list);
-        fragementNewsPresenter = new fragementNewsPresenter(getContext(),this);
+        fragementNewsPresenter = new fragementNewsPresenter(getContext(),this,list);
     }
 }
